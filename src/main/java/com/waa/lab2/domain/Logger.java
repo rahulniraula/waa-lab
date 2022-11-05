8bb9659f33148a282927c366fb61dda448f0e854
Rahul Niraula <rahul.niraula@gmail.com>
2022-11-05 16:39:48 -0500

    started lab4

---
+++ src/main/java/com/waa/lab2/domain/Logger.java
@@ -0,0 +1,24 @@
+package com.waa.lab2.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Logger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long transactionId;
+    Date date;
+    LocalDateTime time;
+}
